edcee125adbe1df3e269b70e0f3ac6a0c4aabee0
skiedflakes <46420286+skiedflakes@users.noreply.github.com>
2019-12-26 16:36:20 +0800

    jr gwapo

--- app/src/main/java/skiedflakes/iBlind/MainActivity.java
+++ app/src/main/java/skiedflakes/iBlind/MainActivity.java
@@ -1,23 +1,31 @@
 package skiedflakes.iBlind;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import skiedflakes.iBlind.ui.Login.Login;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+
+
+        navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
+
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
@@ -25,6 +33,19 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        Login login = new Login();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.container, login);
+        fragmentTransaction.commit();
+        navView.setVisibility(View.GONE);
+    }
+
+    public void nav_view(){
+
+        navView.setVisibility(View.GONE);
     }
 
 }
---
+++ app/src/main/java/skiedflakes/iBlind/SessionManager.java
@@ -0,0 +1,127 @@
+package skiedflakes.iBlind;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+
+
+public class SessionManager {
+    // Shared Preferences
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    public SharedPreferences.Editor editor;
+
+    // Context
+    Context _context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Sharedpref file name
+    private static final String PREF_NAME = "userLogin";
+
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
+
+    // User name (make variable public to access from outside)
+    public static final String KEY_PASSWORD = "password";
+
+    // Email address (make variable public to access from outside)
+    public static final String KEY_NAME = "name";
+
+    public static final String KEY_USER_TYPE = "user_type";
+
+    // Constructor
+    public SessionManager(Context context){
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    /**
+     * Create login session
+     * */
+    public void createLoginSession(String name,String user_type){
+
+
+        // Storing email in pref
+        editor.putString(KEY_NAME, name);
+        // Storing branch id
+        editor.putString(KEY_USER_TYPE, user_type);
+
+        // commit changes
+        editor.commit();
+    }
+
+
+
+    /**
+     * Check login method wil check user login status
+     * If false it will redirect user to login page
+     * Else won't do anything
+     * */
+    public void checkLogin(){
+        // Check login status
+        if(!this.isLoggedIn()){
+            // Do nothing
+        } else {
+            // user is not logged in redirect him to Login Activity
+//            Intent i = new Intent(_context, Main.class);
+//            // Closing all the Activities
+//            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//
+//            // Add new Flag to start new Activity
+//            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//
+//            // Staring Login Activity
+//            _context.startActivity(i);
+        }
+    }
+
+
+    /*** Get stored session data
+     * */
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+
+        // name
+        user.put(KEY_NAME, pref.getString(KEY_NAME, null));
+
+        //user type
+        user.put(KEY_USER_TYPE, pref.getString(KEY_USER_TYPE, null));
+
+        // return user
+        return user;
+    }
+
+    /**
+     * Clear session details
+     * */
+    public void logoutUser(){
+//        // Clearing all data from Shared Preferences
+//        editor.clear();
+//        editor.commit();
+//
+//        // After logout redirect user to Loging Activity
+//        Intent i = new Intent(_context, Login.class);
+//        // Closing all the Activities
+//        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//
+//        // Add new Flag to start new Activity
+//        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//
+//        // Staring Login Activity
+//        _context.startActivity(i);
+    }
+
+    /**
+     * Quick check for login
+     * **/
+    // Get Login State
+    public boolean isLoggedIn(){
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+}
